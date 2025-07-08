c6f2298412f6b4d3aed482c7d732b34af8d4d6f1
jogge-sys <georgos_iliopoulos@hotmail.com>
2025-07-08 21:06:25 +0200

    Add files via upload

--- CPPProj/StbImageCSharp.cpp
+++ CPPProj/StbImageCSharp.cpp
@@ -1,8 +1,8 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "libraries/stb_image.h"
-//#define STB_IMAGE_WRITE_IMPLEMENTATION
-//#include "libraries/stb_image_write.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "libraries/stb_image_write.h"
 
 extern "C" {
 
@@ -21,4 +21,20 @@ extern "C" {
 /*	void Image_Free (uint8_t* Data) {
 		stbi_image_free(Data);
 	}*/
+
+	void Write_PNG (char* Path, int Width, int Height, int Channels, uint8_t* Data) {
+		stbi_write_png(Path, Width, Height, Channels, Data, Width * Channels);
+	}
+
+	void Write_JPG (char* Path, int Width, int Height, int Channels, uint8_t* Data, int Quality) {
+		stbi_write_jpg(Path, Width, Height, Channels, Data, Quality);
+	}
+
+	void Write_BMP (char* Path, int Width, int Height, int Channels, uint8_t* Data) {
+		stbi_write_bmp(Path, Width, Height, Channels, Data);
+	}
+
+	void Write_TGA (char* Path, int Width, int Height, int Channels, uint8_t* Data) {
+		stbi_write_tga(Path, Width, Height, Channels, Data);
+	}
 }
